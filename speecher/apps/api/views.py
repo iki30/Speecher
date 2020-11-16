84c002063ab15cf20feb8537eaf0f33b46c653b2
iKi <kachmar123@yandex.ru>
2020-11-16 15:11:06 +0300

    refactored google speech recognition. Now we use api google for speech recognition

--- speecher/apps/api/views.py
+++ speecher/apps/api/views.py
@@ -1,10 +1,18 @@
-import speech_recognition as sr
+import json
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from speecher.apps.api.serializers import SpeechRecognizeSerializer
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+
+class RequestError(Exception):
+    pass
 
 
 class SpeechRecognizeView(viewsets.GenericViewSet):
@@ -27,22 +35,50 @@ class SpeechRecognizeView(viewsets.GenericViewSet):
         )
 
     def get_transcript_by_audio(self, audio, language):
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(audio) as source:
-            audio = recognizer.record(source)
+        data = self.get_audio_to_flac(audio)
+        response = self.recognize_google_api(data, language)
+        return self.normalize_response_to_json(response)
+
+    def recognize_google_api(self, data, language):
+        audio_rate = 48000  # audio sample rate must be between 8 kHz and 48 kHz inclusive
+
+        url = self.generate_speech_api_url(language)
+        request = Request(url, data=data, headers={"Content-Type": "audio/x-flac; rate={}".format(audio_rate)})
+
         try:
-            audio_output = recognizer.recognize_google(
-                audio, language=language, show_all=True
-            )
-        except sr.RequestError:
-            return Response({
-                "error": {"error_message": "API was unreachable or unresponsive"}
-            })
-        except sr.UnknownValueError:
-            return Response({
-                "error": {"error_code": 1, "error_message": "Recognition service return unexpected error"}
-            })
-        return audio_output
+            response = urlopen(request, timeout=10000)
+        except HTTPError as e:
+            raise RequestError("recognition request failed: {}".format(e.reason))
+        except URLError as e:
+            raise RequestError("recognition connection failed: {}".format(e.reason))
+        return response.read().decode("utf-8")
+
+    def get_audio_to_flac(self, audio):
+        """
+        Метод, который преобразует нашу бинарные данные в flac
+        :param audio:
+        :return:
+        """
+        return audio.read()
+
+    def normalize_response_to_json(self, response_text):
+        result = None
+        for line in response_text.split("\n"):
+            temp_result = json.loads(line)["result"]
+            if len(temp_result):
+                result = temp_result[0]
+                break
+        return result
+
+    def generate_speech_api_url(self, language, key=None):
+        key = key or "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"
+        url = "http://www.google.com/speech-api/v2/recognize?{}".format(urlencode({
+            "client": "chromium",
+            "lang": language,
+            "key": key,
+        }))
+
+        return url
 
     def _round_to_two(self, confidence):
         return confidence and round(confidence, 2)

597a3a17968046d8a247021e9e9c53b30d8e650c
iKi <kachmar123@yandex.ru>
2020-11-16 15:17:35 +0300

    Added temp validate by file

--- speecher/apps/api/serializers.py
+++ speecher/apps/api/serializers.py
@@ -7,3 +7,8 @@ class SpeechRecognizeSerializer(serializers.Serializer):
     audio = serializers.FileField()
     language_code = serializers.ChoiceField(choices=LANGUAGES_CODE, default='en-US')
 
+    def validate_audio(self, file):
+        if not file._name.endswith('flac'):
+            raise serializers.ValidationError('Файл должен быть расширения flac')
+
+        return file
